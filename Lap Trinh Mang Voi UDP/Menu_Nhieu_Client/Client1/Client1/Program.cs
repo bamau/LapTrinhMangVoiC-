1a57d7e9bd6710f638c306e01ad06fb914a62e20
bamau <bamau97@gmail.com>
2018-10-18 16:27:21 +0700

    Update menu nhieu client udp

---
+++ Lap Trinh Mang Voi UDP/Menu_Nhieu_Client/Client1/Client1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient("127.0.0.1", (9050));
+            IPEndPoint server = new IPEndPoint(IPAddress.Any, 0);
+            int dem = 0;
+            Console.WriteLine("Nhap vao ba so nguyen:");
+            while (dem < 3)
+            {
+                dem++;
+                Console.Write("So " + dem + ": ");
+                string so1 = Console.ReadLine() + "_" + dem;
+                byte[] data1 = Encoding.ASCII.GetBytes(so1);
+                client.Send(data1, data1.Length);
+            }
+            Console.Clear();
+            byte[] data = new byte[10];
+            data = client.Receive(ref server);
+            string menu = Encoding.ASCII.GetString(data);
+            string[] menucut = menu.Split(',');
+            for (int i = 0; i < menucut.Length; i++)
+            {
+                Console.WriteLine(menucut[i]);
+            }
+            string so2;
+            do
+            {
+                Console.Write("Nhap lua chon: ");
+                so2 = Console.ReadLine();
+                byte[] data2 = Encoding.ASCII.GetBytes(so2);
+                client.Send(data2, data2.Length);
+                if(so2=="1")
+                {
+                    byte[] data3 = new byte[10];
+                    data3 = client.Receive(ref server);
+                    string kq = Encoding.ASCII.GetString(data3);
+                    Console.WriteLine(kq);
+                }
+                if(so2=="2")
+                {
+
+                }
+            } while (so2 != "3");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lap Trinh Mang Voi UDP/Menu_Nhieu_Client/Server/Server/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient(9050);
+            List<IPEndPoint> listclient = new List<IPEndPoint>();
+            List<int[]> listso = new List<int[]>();
+            List<int> listsoluong = new List<int>();
+            IPEndPoint client = new IPEndPoint(IPAddress.Any, 0);
+           
+            while (true)
+            {
+                byte[] data1 = new byte[10];
+                data1 = server.Receive(ref client);
+                string so = Encoding.ASCII.GetString(data1, 0, data1.Length);
+                string[] cutnumber =so.Split('_');
+                int a = int.Parse(cutnumber[0]);              
+                int i;
+                int[] arr = new int[3];
+                for (i = 0; i < listclient.Count; i++)
+                {
+                    if(client.Equals(listclient.ElementAt(i)))
+                    {
+                        listsoluong[i]++;
+                        if(listsoluong.ElementAt(i)==3)
+                        {
+                            string menu = "1.Tong ba so,2.Cac so le co trong ba so,3.Thoat";
+                            byte[] data = new byte[10];
+                            data = Encoding.ASCII.GetBytes(menu);                           
+                            server.Send(data, data.Length, listclient.ElementAt(i));
+
+                            byte[] data2 = new byte[10];
+                            data2 = server.Receive(ref client);
+                            string so1 = Encoding.ASCII.GetString(data2, 0, data2.Length);
+                            if(so1=="1")
+                            {
+                                int kq = arr[0]+arr[1]+arr[2];
+                                byte[] data3 = new byte[10];
+                                data3 = Encoding.ASCII.GetBytes(kq.ToString());
+                                server.Send(data3, data3.Length, listclient.ElementAt(i));
+                            }
+                        }
+                        if(listsoluong.ElementAt(i)<3)
+                        {
+                            
+                            if(cutnumber[1]=="1")
+                            {                              
+                                arr[0] = a;
+                            }
+                            if (cutnumber[1] == "2")
+                            {                               
+                                arr[1] = a;
+                            }
+                            if (cutnumber[1] == "3")
+                            {                               
+                                arr[2] = a;
+                            }
+                           
+                        }
+                    }
+                   
+                }
+                if (i == listclient.Count)
+                {
+                    listclient.Add(client);                              
+                    listsoluong.Add(1);                   
+                    if (cutnumber[1] == "1")
+                    {                        
+                        arr[0] = a;
+                        listso.Add(arr);                                             
+                    }
+                    if (cutnumber[1] == "2")
+                    {
+                        arr[1] = a;
+                        listso.Add(arr);
+                    }
+                    if (cutnumber[1] == "3")
+                    {
+                        arr[2] = a;
+                        listso.Add(arr);
+                    }
+                }
+                
+            }
+        }
+    }
+}
